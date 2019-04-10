d1dffd16301eea4c024787add2cf86baffa22830
lingdian <xuhuawei131@126.com>
2019-04-11 01:08:03 +0800

    Initial commit

---
+++ app/src/main/java/com/ricky/materialdesign/custombehavior/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ricky.materialdesign.custombehavior;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+	private TextView tv1;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	}
+
+}
